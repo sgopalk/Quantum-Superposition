8c937df4e8fba5ad8e61aa0ab8f419008c7017a4
Gopal Kashyap <157359816+sgopalk@users.noreply.github.com>
2025-09-01 19:22:32 +0530

    Update quantum_superposition.py

--- quantum_superposition.py
+++ quantum_superposition.py
@@ -236,7 +236,7 @@ for i in range(N):
 # Show normalization constant input
 st.sidebar.markdown("### Normalization Factor")
 #row1 = st.sidebar.rows(1)
-A = st.sidebar.number_input(f"A", value=1.0,format="%.3f" )
+A = st.sidebar.number_input(f"A", value=1.0,step=0.01, format="%.3f" )
 if potential == "1D Infinite Square Well": 
     st.sidebar.markdown("### Length of Box")
     L= st.sidebar.number_input(f"L",min_value=0.5,max_value=10.0,value=1.0,step=0.5 )#1  # Or get this from user input if needed
